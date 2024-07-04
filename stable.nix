15a6a1a7852169cb90bf3d0623e1366f39833233
Calvin Loncaric <marvinx03@gmail.com>
2024-07-03 20:00:02 -0700

    Upgrade stable dependencies

--- stable.nix
+++ stable.nix
@@ -1,13 +1,13 @@
 import ./default.nix rec {
 
   nixpkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/abd6d48f8c77.tar.gz";
-    sha256 = "0lisfh5b3dgcjb083nsxmffvzxzs6ir1jfsxfaf29gjpjnv7sm7f";
+    url = "https://github.com/NixOS/nixpkgs/archive/0aeab749216e4c073cece5d34bc01b79e717c3e0.tar.gz";
+    sha256 = "0fnw58pxb9l2iskrcr7d03vkbzf19qqb23r7w0ds2a62wclzxc6h";
   }) {};
 
   nixjars = import (builtins.fetchTarball {
-    url = "https://github.com/Calvin-L/nixjars/archive/adbc4e3ff414009b54b7616c00d7781f3e9cb548.tar.gz";
-    sha256 = "1jklkdky2d4y42ljd0nj5la1x6al92zm39sk9h367zrazn8140lc";
+    url = "https://github.com/Calvin-L/nixjars/archive/36f19d336e00d45c8f2636b4938b962277c02c2d.tar.gz";
+    sha256 = "0l5n1iky7nl6gqfbksfkvp24lmi9c1l81ci7lpyspa2fffp50blv";
   }) {nixpkgs=nixpkgs;};
 
 }
