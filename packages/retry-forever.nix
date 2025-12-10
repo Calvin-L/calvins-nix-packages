245e8b7e0adbbdcd6076ca6be5b5d3755d4c9c8f
Calvin Loncaric <marvinx03@gmail.com>
2025-12-10 13:41:16 -0800

    Update retry-forever

--- packages/retry-forever.nix
+++ packages/retry-forever.nix
@@ -6,8 +6,8 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "Calvin-L";
     repo = "retry-forever";
-    rev = "347ea21bcc9a422b11f1c41f3d960aacd3cfd671";
-    sha256 = "14k1z875fgannj5z0w2k25d97pd2i9vhpc2walmpbb44l9bij7yr";
+    rev = "076fdc60109ae78c214032be9c32f1faf43e3b9c";
+    hash = "sha256-jfBcifKXB5Qf0jPz5lgzNvAuJLE1ZERHA4IE5DYEjvM=";
   };
   nativeBuildInputs = [
     cmake
